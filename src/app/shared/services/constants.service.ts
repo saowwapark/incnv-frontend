4278959fdbf908c00e850a3c845dcff504cb6706
Sakkayaphab Piwluang <sakkayaphab@gmail.com>
2020-02-27 01:07:35 +0700

    update

--- src/app/shared/services/constants.service.ts
+++ src/app/shared/services/constants.service.ts
@@ -2,5 +2,5 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ConstantsService {
-  readonly baseAppUrl: string = 'http://localhost:3000';
+  readonly baseAppUrl: string = 'http://incnv@ec2-52-74-3-82.ap-southeast-1.compute.amazonaws.com:7001';
 }
