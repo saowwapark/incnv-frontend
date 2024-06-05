77cf788bfd8f500e2e2f35d7b9c8203240258717
Saowwapark Chanwigoon <mew.plays@gmail.com>
2024-06-05 19:16:43 +0700

    Fix banner position in My Files page

--- src/app/shared/components/selected-bar/selected-bar.component.ts
+++ src/app/shared/components/selected-bar/selected-bar.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 import { SelectedBarType } from './selected-bar-type.model';
 
 @Component({
-  selector: 'app-selected-bar',
+  selector: 'selected-bar',
   templateUrl: './selected-bar.component.html',
   styleUrls: ['./selected-bar.component.scss']
 })
@@ -24,26 +24,3 @@ export class SelectedBarComponent {
     this.submitEmitter.next(this.selectedData);
   }
 }
-// export class SelectedBarComponent implements OnInit {
-//   @Input() service: SelectedBarInterface<any>;
-//   @Input() selectedBarType: number;
-//   SelectedBarType = SelectedBarType;
-//   selectedRows: any[];
-//   confirmDialogRef: MatDialogRef<ConfirmDialogComponent>;
-
-//   rowLength: number;
-//   constructor(public _matDialog: MatDialog) {}
-//   ngOnInit() {
-//     this.service.onSelectedChanged.subscribe(selectedRows => {
-//       this.rowLength = selectedRows.length;
-//     });
-//   }
-//   deselectAll() {
-//     // Trigger the next event
-//     this.service.onSelectedChanged.next([]);
-//   }
-
-//   onSubmitAllSelected() {
-//     this.service.submitAllSelected();
-//   }
-// }
