2ad60164ca26c1534b48b747fda15897af6e0420
Saowwapark Chanwigoon <mew.plays@gmail.com>
2024-06-10 14:51:47 +0700

    [AT-584] - [Bug] Add Authen to all child routing paths

--- src/app/sampleset/sampleset-routing.module.ts
+++ src/app/sampleset/sampleset-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SamplesetComponent } from './sampleset.component';
-import { AuthenGuard } from '../authen/authen.guard';
 import { SamplesetResolver } from './sampleset.resolver';
 
 const routes: Routes = [
   {
     path: '',
     component: SamplesetComponent,
-    // canActivate: [AuthenGuard],
     resolve: { samplesets: SamplesetResolver }
   },
   {
